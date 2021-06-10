d56577395a980b942767400b8968ad721d450691
Minsu Kim <minsu.kim@digipen.edu>
2021-06-10 22:39:45 +0900

    Edit file name Image.js->Preload.js
    Acutally, preload also used for sound. So, I changed it

--- Preload.js
+++ Preload.js
@@ -1,8 +1,12 @@
+// Image
 let HomeImage;
 let PointImage;
 
+// Sound
+
 function preload()
 {
+    // Load Image
     HomeImage = loadImage( 'assets/Image/home.png' )
     PointImage = loadImage( 'assets/Image/point.png' )
 }
