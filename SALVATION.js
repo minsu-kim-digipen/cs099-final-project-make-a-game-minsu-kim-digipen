61bb45cfe80a012b2201661f1b5cee65a57a4e8b
Minsu Kim <minsu.kim@digipen.edu>
2021-06-26 15:59:37 +0900

    Add comment

--- SALVATION.js
+++ SALVATION.js
@@ -3,16 +3,20 @@
 // Course     : CS099
 // Spring 2021
 
+// Use in fill salvation
 let Salvation_Point = 0;
+
+let Fill_MIN_salvation = 0;
+let Fill_MAX_salvation = 100;
+
+// Use in salvation chance
+let SALVATION = false;
 
 let MIN_salvation = 0;
 let MAX_salvation = 100;
 let DEBT_salvation = 50;
 
-let Fill_MIN_salvation = 0;
-let Fill_MAX_salvation = 100;
 
 let GET_SALVATION = 0;
 let Debt = 0;
 
-let SALVATION = false;
